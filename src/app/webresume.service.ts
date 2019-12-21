e69a01d51614c1fb9dda11c677740f222d012f43
CodePlexbofcarbon1 <bofcarbon1@gmail.com>
2019-12-20 18:09:23 -0700

    reduce observable pipe http delay in webresume.service.ts

--- src/app/webresume.service.ts
+++ src/app/webresume.service.ts
@@ -86,7 +86,7 @@ export class WRService {
     public GetSkillsByType = (skilltype): Observable<any> => {
         return this.http.get(this.actionUrlSkills + skilltype)
             .map(response => response.json())
-            .pipe(delay(3000))
+            .pipe(delay(500))
             .catch(this.handleError);
     }
     
